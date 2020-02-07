a45f343c9f9e7f041dde5de5c812b6483bd04ac6
karikera <karikera3@hotmail.com>
2020-02-07 16:02:35 +0900

    first commit

---
+++ wv2debug/wv2debug.cpp
@@ -0,0 +1,33 @@
+
+#include <sstream>
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+using namespace std;
+
+int main(int argn, char** argv, char **env) {
+
+	stringstream ss;
+
+	for (int i = 1; i < argn; i++) {
+		ss << "argv[" << i << "]: " << argv[i] << endl;
+	}
+
+	char env_entry[1024];
+	size_t size = sizeof(env_entry);
+	
+	getenv_s(&size, env_entry, "WV_ENTRY");
+	ss << "WV_ENTRY: " << env_entry << endl;
+
+	char cwd[1024];
+	GetCurrentDirectoryA(MAX_PATH, cwd);
+	ss << "cwd: " << cwd << endl;
+
+	MessageBoxA(nullptr, ss.str().c_str(), nullptr, MB_OK);
+
+	// Just for testing WebView2 Debugging
+	// No need to write real WebView2 codes
+
+	return 0;
+}
